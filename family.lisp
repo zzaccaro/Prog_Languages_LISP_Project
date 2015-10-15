42ebd7cee5c49dda2de8e443d5fa1b41b04419bf
Zach Zaccaro <zzaccaro@villanova.edu>
2015-10-15 14:39:02 -0400

    implemented some boolean methods

--- family.lisp
+++ family.lisp
@@ -16,12 +16,12 @@
 
 ;function to add new child
 (defun add-children (p newchild) 
-	(nconc (person-children p) (list newchild))
+	(setf (person-children p) (nconc (person-children p) (list newchild)))
 	)
 
 ;function to add parent
 (defun add-parent (p newparent)
-	(nconc (person-parents p) (list newparent))
+	(setf (person-parents p) (nconc (person-parents p) (list newparent)))
 	)
 
 ;function to set parents
@@ -31,10 +31,10 @@
 
 ;function to add spouse
 (defun add-spouse (p spouse)
-	(nconc (person-spouse p) (list spouse))
+	(setf (person-spouse p) (nconc (person-spouse p) (list spouse)))
 	)
 
-
+(defvar familytree (make-hash-table))
 
 ;; main function
 (defun family ()
@@ -55,10 +55,16 @@
 
 	)
 
+(defun checkOrAddToGraph (nodeName p1 p2)
+	
+
+	)
+
 ;; function to check if person is part of Adam and Eve generation
-(defun isAdamAndEve ()
-
-
+(defun isAdamAndEve (name)
+	(if ((not (member name (person-parents (gethash name familytree)) ) ))
+		nil 
+		t)
 	)
 
 ;; R query
@@ -68,28 +74,55 @@
 	)
 
 ;; boolean for spouse
-(defun isSpouse ()
-
-
+(defun isSpouse (p1 p2)
+	(if (not (member (person-name p1) (person-spouse p2)))
+		nil 
+		t)
 	)
 
 ;; boolean for parent
-(defun isParent ()
+(defun isParent (p1 p2)
+	(if (equalp (person-name p1) (person-name p2)) (return-from nil))
 
-	
+	(if (not (member (person-name p1) (person-parents p2)))
+		nil 
+		t)
+	)
+
+;; boolean for sibling
+(defun isSibling (p1 p2)
+
 	)
 
 ;; boolean for ancestor
-(defun isAncestor ()
+(defun isAncestor (p1 p2)
+	;; check for same name first...
 
-	
+	(if (checkParents(p1 p2))
+		(return-from t) 
+		(return-from nil)
 	)
+)
 
 ;; recursive method for isAncestor
-(defun checkParents ()
-
-	
+(defun checkParents (target p)
+	(if (isAdamAndEve((person-name p)))
+		(return-from nil) 
+		(if ((equalp (first (person-parents p)) (person-name target)))
+			(return-from t) 
+			(if ((equalp (second (person-parents p)) (person-name target)))
+				(return-from t) 
+				(if ((checkParents(target (gethash (first (person-parents p)) familytree))))
+					(return-from t) 
+					(if ((checkParents(target (gethash (second (person-parents p)) familytree))))
+						(return-from t) 
+						(return-from nil)
+					)
+				)
+			)
+		)
 	)
+)
 
 ;; boolean for cousin
 (defun isCousin ()
