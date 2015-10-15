6e138537ee81fb3316d8419de4ae2d5b117dfff6
Zach Zaccaro <zzaccaro@villanova.edu>
2015-10-14 20:54:39 -0400

    outlined functions

--- family.lisp
+++ family.lisp
@@ -1,3 +1,7 @@
+;; people object
+()
+
+
 ;; main function
 (defun family ()
 
@@ -10,3 +14,93 @@
 ;		(setf a (read __ ))
 ;		(format t " " _ _ _)
 ;		))
+
+;; E query
+(defun E ()
+
+
+	)
+
+;; function to check if person is part of Adam and Eve generation
+(defun isAdamAndEve ()
+
+
+	)
+
+;; R query
+(defun R ()
+
+
+	)
+
+;; boolean for spouse
+(defun isSpouse ()
+
+
+	)
+
+;; boolean for parent
+(defun isParent ()
+
+	
+	)
+
+;; boolean for ancestor
+(defun isAncestor ()
+
+	
+	)
+
+;; recursive method for isAncestor
+(defun checkParents ()
+
+	
+	)
+
+;; boolean for cousin
+(defun isCousin ()
+
+	
+	)
+
+;; boolean for relative
+(defun isRelative ()
+
+	
+	)
+
+;; X query
+(defun X ()
+
+
+	)
+
+;; W query
+(defun W ()
+
+
+	)
+
+;; collects all people who are siblings of a person
+(defun getSiblings ()
+
+
+	)
+
+;; collects all people who are ancestors of a person
+(defun getAncestors ()
+
+
+	)
+
+;; collects all people who are relatives of a person
+(defun getRelatives ()
+
+
+	)
+
+;; collects all people who are not related to a person
+(defun getStrangers ()
+
+
+	)
