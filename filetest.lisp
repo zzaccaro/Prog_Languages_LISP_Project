a7371bb03eaa878cda79db4315834466e2d9c84c
Jason <future.elements247@gmail.com>
2015-10-15 12:25:38 -0400

    Wrote potential file reader

---
+++ filetest.lisp
@@ -0,0 +1,9 @@
+(with-open-file (str *STANDARD-INPUT*
+                     :direction :INPUT
+                     ;:if-exists <if-exists>
+                     :if-does-not-exist :ERROR)
+  	(loop for line = (read-line str nil)
+  		while line do ())
+
+
+  	)
